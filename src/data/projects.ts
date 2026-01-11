309b0911926200020e051b6d2c2e5c30998d5cb6
alvaro <alvarord519@gmail.com>
2026-01-11 13:19:00 -0500

    feat: replace JPG images with WEBP format for improved performance and update ProjectCard and ProjectGallery components

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,49 +1,49 @@
 import type { ImageMetadata } from 'astro';
 
 // Proyecto 01
-import p01_01 from '../assets/proyectos/01/01.jpg';
-import p01_02 from '../assets/proyectos/01/02.jpg';
-import p01_03 from '../assets/proyectos/01/03.jpg';
-import p01_04 from '../assets/proyectos/01/04.jpg';
-import p01_05 from '../assets/proyectos/01/05.jpg';
-import p01_06 from '../assets/proyectos/01/06.jpeg';
+import p01_01 from '../assets/proyectos/01/01.webp';
+import p01_02 from '../assets/proyectos/01/02.webp';
+import p01_03 from '../assets/proyectos/01/03.webp';
+import p01_04 from '../assets/proyectos/01/04.webp';
+import p01_05 from '../assets/proyectos/01/05.webp';
+import p01_06 from '../assets/proyectos/01/06.webp';
 
 // Proyecto 02
-import p02_01 from '../assets/proyectos/02/01.jpg';
-import p02_02 from '../assets/proyectos/02/02.jpg';
-import p02_03 from '../assets/proyectos/02/03.jpg';
-import p02_04 from '../assets/proyectos/02/04.jpg';
-import p02_05 from '../assets/proyectos/02/05.jpg';
-import p02_06 from '../assets/proyectos/02/06.jpg';
-import p02_07 from '../assets/proyectos/02/07.jpg';
-import p02_08 from '../assets/proyectos/02/08.png';
-import p02_09 from '../assets/proyectos/02/09.jpg';
-import p02_10 from '../assets/proyectos/02/10.jpg';
+import p02_01 from '../assets/proyectos/02/01.webp';
+import p02_02 from '../assets/proyectos/02/02.webp';
+import p02_03 from '../assets/proyectos/02/03.webp';
+import p02_04 from '../assets/proyectos/02/04.webp';
+import p02_05 from '../assets/proyectos/02/05.webp';
+import p02_06 from '../assets/proyectos/02/06.webp';
+import p02_07 from '../assets/proyectos/02/07.webp';
+import p02_08 from '../assets/proyectos/02/08.webp';
+import p02_09 from '../assets/proyectos/02/09.webp';
+import p02_10 from '../assets/proyectos/02/10.webp';
 
 // Proyecto 03
-import p03_01 from '../assets/proyectos/03/01.jpg';
-import p03_02 from '../assets/proyectos/03/02.jpg';
-import p03_03 from '../assets/proyectos/03/03.jpg';
-import p03_04 from '../assets/proyectos/03/04.jpg';
-import p03_05 from '../assets/proyectos/03/05.jpg';
+import p03_01 from '../assets/proyectos/03/01.webp';
+import p03_02 from '../assets/proyectos/03/02.webp';
+import p03_03 from '../assets/proyectos/03/03.webp';
+import p03_04 from '../assets/proyectos/03/04.webp';
+import p03_05 from '../assets/proyectos/03/05.webp';
 
 // Proyecto 04
-import p04_01 from '../assets/proyectos/04/01.jpeg';
+import p04_01 from '../assets/proyectos/04/01.webp';
 import p04_02 from '../assets/proyectos/04/02.webp';
-import p04_03 from '../assets/proyectos/04/03.jpeg';
-import p04_04 from '../assets/proyectos/04/04.png';
-import p04_05 from '../assets/proyectos/04/05.png';
+import p04_03 from '../assets/proyectos/04/03.webp';
+import p04_04 from '../assets/proyectos/04/04.webp';
+import p04_05 from '../assets/proyectos/04/05.webp';
 import video04 from '../assets/proyectos/04/video-01.mp4';
 
 // Proyecto 05
-import p05_01 from '../assets/proyectos/05/01.jpg';
-import p05_02 from '../assets/proyectos/05/02.jpg';
-import p05_03 from '../assets/proyectos/05/03.jpg';
-import p05_04 from '../assets/proyectos/05/04.jpg';
-import p05_05 from '../assets/proyectos/05/05.jpg';
-import p05_06 from '../assets/proyectos/05/06.jpg';
-import p05_07 from '../assets/proyectos/05/07.jpg';
-import p05_08 from '../assets/proyectos/05/08.jpg';
+import p05_01 from '../assets/proyectos/05/01.webp';
+import p05_02 from '../assets/proyectos/05/02.webp';
+import p05_03 from '../assets/proyectos/05/03.webp';
+import p05_04 from '../assets/proyectos/05/04.webp';
+import p05_05 from '../assets/proyectos/05/05.webp';
+import p05_06 from '../assets/proyectos/05/06.webp';
+import p05_07 from '../assets/proyectos/05/07.webp';
+import p05_08 from '../assets/proyectos/05/08.webp';
 import video05 from '../assets/proyectos/05/video-01.mp4';
 
 export interface Project {
