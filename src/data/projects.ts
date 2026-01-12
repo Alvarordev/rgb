18ca2be1435db64f96023b9c30d49b20b52a2cae
alvaro <alvarord519@gmail.com>
2026-01-12 00:22:14 -0500

    feat: changed order of projects and updated image layout

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,50 +1,50 @@
-import type { ImageMetadata } from 'astro';
+import type { ImageMetadata } from "astro";
 
 // Proyecto 01
-import p01_01 from '../assets/proyectos/01/01.webp';
-import p01_02 from '../assets/proyectos/01/02.webp';
-import p01_03 from '../assets/proyectos/01/03.webp';
-import p01_04 from '../assets/proyectos/01/04.webp';
-import p01_05 from '../assets/proyectos/01/05.webp';
-import p01_06 from '../assets/proyectos/01/06.webp';
+import p01_01 from "../assets/proyectos/01/01.webp";
+import p01_02 from "../assets/proyectos/01/02.webp";
+import p01_03 from "../assets/proyectos/01/03.webp";
+import p01_04 from "../assets/proyectos/01/04.webp";
+import p01_05 from "../assets/proyectos/01/05.webp";
+import p01_06 from "../assets/proyectos/01/06.webp";
 
 // Proyecto 02
-import p02_01 from '../assets/proyectos/02/01.webp';
-import p02_02 from '../assets/proyectos/02/02.webp';
-import p02_03 from '../assets/proyectos/02/03.webp';
-import p02_04 from '../assets/proyectos/02/04.webp';
-import p02_05 from '../assets/proyectos/02/05.webp';
-import p02_06 from '../assets/proyectos/02/06.webp';
-import p02_07 from '../assets/proyectos/02/07.webp';
-import p02_08 from '../assets/proyectos/02/08.webp';
-import p02_09 from '../assets/proyectos/02/09.webp';
-import p02_10 from '../assets/proyectos/02/10.webp';
+import p02_01 from "../assets/proyectos/02/01.webp";
+import p02_02 from "../assets/proyectos/02/02.webp";
+import p02_03 from "../assets/proyectos/02/03.webp";
+import p02_04 from "../assets/proyectos/02/04.webp";
+import p02_05 from "../assets/proyectos/02/05.webp";
+import p02_06 from "../assets/proyectos/02/06.webp";
+import p02_07 from "../assets/proyectos/02/07.webp";
+import p02_08 from "../assets/proyectos/02/08.webp";
+import p02_09 from "../assets/proyectos/02/09.webp";
+import p02_10 from "../assets/proyectos/02/10.webp";
 
 // Proyecto 03
-import p03_01 from '../assets/proyectos/03/01.webp';
-import p03_02 from '../assets/proyectos/03/02.webp';
-import p03_03 from '../assets/proyectos/03/03.webp';
-import p03_04 from '../assets/proyectos/03/04.webp';
-import p03_05 from '../assets/proyectos/03/05.webp';
+import p03_01 from "../assets/proyectos/03/01.webp";
+import p03_02 from "../assets/proyectos/03/02.webp";
+import p03_03 from "../assets/proyectos/03/03.webp";
+import p03_04 from "../assets/proyectos/03/04.webp";
+import p03_05 from "../assets/proyectos/03/05.webp";
 
 // Proyecto 04
-import p04_01 from '../assets/proyectos/04/01.webp';
-import p04_02 from '../assets/proyectos/04/02.webp';
-import p04_03 from '../assets/proyectos/04/03.webp';
-import p04_04 from '../assets/proyectos/04/04.webp';
-import p04_05 from '../assets/proyectos/04/05.webp';
-import video04 from '../assets/proyectos/04/video-01.mp4';
+import p04_01 from "../assets/proyectos/04/01.webp";
+import p04_02 from "../assets/proyectos/04/02.webp";
+import p04_03 from "../assets/proyectos/04/03.webp";
+import p04_04 from "../assets/proyectos/04/04.webp";
+import p04_05 from "../assets/proyectos/04/05.webp";
+import video04 from "../assets/proyectos/04/video-01.mp4";
 
 // Proyecto 05
-import p05_01 from '../assets/proyectos/05/01.webp';
-import p05_02 from '../assets/proyectos/05/02.webp';
-import p05_03 from '../assets/proyectos/05/03.webp';
-import p05_04 from '../assets/proyectos/05/04.webp';
-import p05_05 from '../assets/proyectos/05/05.webp';
-import p05_06 from '../assets/proyectos/05/06.webp';
-import p05_07 from '../assets/proyectos/05/07.webp';
-import p05_08 from '../assets/proyectos/05/08.webp';
-import video05 from '../assets/proyectos/05/video-01.mp4';
+import p05_01 from "../assets/proyectos/05/01.webp";
+import p05_02 from "../assets/proyectos/05/02.webp";
+import p05_03 from "../assets/proyectos/05/03.webp";
+import p05_04 from "../assets/proyectos/05/04.webp";
+import p05_05 from "../assets/proyectos/05/05.webp";
+import p05_06 from "../assets/proyectos/05/06.webp";
+import p05_07 from "../assets/proyectos/05/07.webp";
+import p05_08 from "../assets/proyectos/05/08.webp";
+import video05 from "../assets/proyectos/05/video-01.mp4";
 
 export interface Project {
   id: number;
@@ -60,19 +60,29 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
-    slug: 'bano-sevilla',
-    title: 'Baño Sevilla',
-    year: '2024',
-    location: 'Surco, Lima, Perú',
-    images: [p01_01, p01_02, p01_03, p01_04, p01_05, p01_06],
-    alt: 'Baño Sevilla',
+    slug: "proyecto-c1",
+    title: "Proyecto C1",
+    year: "2025",
+    location: "San Borja, Lima, Perú",
+    images: [p05_01, p05_02, p05_03, p05_04, p05_05, p05_06, p05_07, p05_08],
+    videos: [video05],
+    alt: "Proyecto C1",
   },
   {
     id: 2,
-    slug: 'departamento-elba',
-    title: 'Departamento Elba',
-    year: '2025',
-    location: 'Trujillo, Perú',
+    slug: "bano-sevilla",
+    title: "Baño Sevilla",
+    year: "2024",
+    location: "Surco, Lima, Perú",
+    images: [p01_01, p01_02, p01_03, p01_04, p01_05, p01_06],
+    alt: "Baño Sevilla",
+  },
+  {
+    id: 3,
+    slug: "departamento-elba",
+    title: "Departamento Elba",
+    year: "2025",
+    location: "Trujillo, Perú",
     images: [
       p02_01,
       p02_02,
@@ -85,44 +95,25 @@ export const projects: Project[] = [
       p02_09,
       p02_10,
     ],
-    alt: 'Depa Elba',
-  },
-  {
-    id: 3,
-    slug: 'patio-atenas',
-    title: 'Patio Atenas',
-    year: '2025',
-    location: 'Los Olivos, Lima, Perú',
-    images: [p03_04, p03_02, p03_03, p03_01, p03_05],
-    alt: 'Patio Atenas',
+    alt: "Depa Elba",
   },
   {
     id: 4,
-    slug: 'terraza-atenas',
-    title: 'Terraza Atenas',
-    year: '2025',
-    location: 'Los Olivos, Lima, Perú',
-    images: [p04_05, p04_02, p04_01, p04_04, p04_03],
-    videos: [video04],
-    alt: 'Terraza Atenas',
+    slug: "patio-atenas",
+    title: "Patio Atenas",
+    year: "2025",
+    location: "Los Olivos, Lima, Perú",
+    images: [p03_04, p03_02, p03_03, p03_01, p03_05],
+    alt: "Patio Atenas",
   },
   {
     id: 5,
-    slug: 'proyecto-c1',
-    title: 'Proyecto C1',
-    year: '2025',
-    location: 'San Borja, Lima, Perú',
-    images: [
-      p05_01,
-      p05_02,
-      p05_03,
-      p05_04,
-      p05_05,
-      p05_06,
-      p05_07,
-      p05_08,
-    ],
-    videos: [video05],
-    alt: 'Proyecto C1',
+    slug: "terraza-atenas",
+    title: "Terraza Atenas",
+    year: "2025",
+    location: "Los Olivos, Lima, Perú",
+    images: [p04_05, p04_02, p04_01, p04_04],
+    videos: [video04],
+    alt: "Terraza Atenas",
   },
 ];
