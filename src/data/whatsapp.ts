9c54b76edcc96e571299ada197e46f3a037ff223
alvaro <alvarord519@gmail.com>
2026-01-20 12:51:17 -0500

    refactor: added whatsapp link on contact us header button and floating menu

---
+++ src/data/whatsapp.ts
@@ -0,0 +1,4 @@
+const phoneNumber = "51934887139";
+const message =
+  "Hola, estoy interesado en realizar un proyecto de diseño con ustedes. Quisiera más información sobre cómo trabajan.";
+export const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
