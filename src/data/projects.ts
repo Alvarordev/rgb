9dc51e909c48c6a974fc3eacd1e804e870e33d12
alvaro <alvarord519@gmail.com>
2026-05-30 18:53:24 -0500

    added new proyects and reorganized exisiting

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -46,6 +46,31 @@ import p05_07 from "../assets/proyectos/05/07.webp";
 import p05_08 from "../assets/proyectos/05/08.webp";
 import video05 from "../assets/proyectos/05/video-01.mp4";
 
+import p06_01 from "../assets/proyectos/06/01.webp";
+import p06_02 from "../assets/proyectos/06/02.webp";
+import p06_03 from "../assets/proyectos/06/03.webp";
+import p06_04 from "../assets/proyectos/06/04.webp";
+import p06_05 from "../assets/proyectos/06/05.webp";
+import p06_06 from "../assets/proyectos/06/06.webp";
+import p06_07 from "../assets/proyectos/06/07.webp";
+import p06_08 from "../assets/proyectos/06/08.webp";
+import p06_09 from "../assets/proyectos/06/09.webp";
+import p06_10 from "../assets/proyectos/06/10.webp";
+import p06_11 from "../assets/proyectos/06/11.webp";
+import p06_12 from "../assets/proyectos/06/12.webp";
+import p06_13 from "../assets/proyectos/06/13.webp";
+import p06_14 from "../assets/proyectos/06/14.webp";
+
+import p07_01 from "../assets/proyectos/07/01.webp";
+import p07_02 from "../assets/proyectos/07/02.webp";
+import p07_03 from "../assets/proyectos/07/03.webp";
+import p07_04 from "../assets/proyectos/07/04.webp";
+import p07_05 from "../assets/proyectos/07/05.webp";
+import p07_06 from "../assets/proyectos/07/06.webp";
+import p07_07 from "../assets/proyectos/07/07.webp";
+import p07_08 from "../assets/proyectos/07/08.webp";
+import p07_09 from "../assets/proyectos/07/09.webp";
+
 export interface Project {
   id: number;
   slug: string;
@@ -60,6 +85,15 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
+    slug: "cocina",
+    title: "Cocina",
+    year: "2026",
+    location: "San Borja, Lima, Perú",
+    images: [p07_06, p07_02, p07_04, p07_08, p07_03, p07_01, p07_07, p07_05, p07_09],
+    alt: "Cocina",
+  },
+  {
+    id: 2,
     slug: "proyecto-c1",
     title: "Proyecto C1",
     year: "2025",
@@ -69,7 +103,7 @@ export const projects: Project[] = [
     alt: "Proyecto C1",
   },
   {
-    id: 2,
+    id: 3,
     slug: "bano-sevilla",
     title: "Baño Sevilla",
     year: "2024",
@@ -78,7 +112,7 @@ export const projects: Project[] = [
     alt: "Baño Sevilla",
   },
   {
-    id: 3,
+    id: 4,
     slug: "departamento-elba",
     title: "Departamento Elba",
     year: "2025",
@@ -98,7 +132,7 @@ export const projects: Project[] = [
     alt: "Depa Elba",
   },
   {
-    id: 4,
+    id: 5,
     slug: "patio-atenas",
     title: "Patio Atenas",
     year: "2025",
@@ -107,7 +141,16 @@ export const projects: Project[] = [
     alt: "Patio Atenas",
   },
   {
-    id: 5,
+    id: 6,
+    slug: "proyecto-nilda",
+    title: "Proyectoo Nilda",
+    year: "2026",
+    location: "San Borja, Lima, Perú",
+    images: [p06_01, p06_02, p06_03, p06_04,p06_05, p06_06, p06_07, p06_08, p06_09, p06_10, p06_11, p06_12, p06_13, p06_14],
+    alt: "Proyecto Nilda",
+  },
+  {
+    id: 7,
     slug: "terraza-atenas",
     title: "Terraza Atenas",
     year: "2025",
